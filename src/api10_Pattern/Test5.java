1cf375b85b082a26722002b0209296d54d893b01
minimum <pt_choi5246@naver.com>
2024-03-15 17:05:52 +0900

    20240315 1705 0313api

--- src/api10_Pattern/Test5.java
+++ src/api10_Pattern/Test5.java
@@ -2,45 +2,44 @@ package api10_Pattern;
 
 import java.util.regex.Pattern;
 
-// 문자열 정규식 // '?'는 0번이나 1번 '+'는 1번 이상 '*'는 0번 이상
-public class Test4 {
+// 문자열 정규식 // '?'는 0번이나 1번 '+'는 1번 이상 '*'는 0번 이상 //?+* 대신에 {3, :글자 이상 12, :글자 이하}길이
+//\\w 영문 대소문자 / 숫자 /밑줄_/ 허용
+public class Test5 {
 	public static void main(String[] args) {
 		
-		String regEx1 = "^[abc]*$"; //a 또는 b또는 c 문자만 포함? ^[abc]*$, ^[a|b|c]*$
-		//a로 시작해서 끝나야 하고 b로 시작해서 끝나야하고 c로 시작해서 끝나야하는것
-		//시작과 끝나는 문자에 a,b,c 가 포함되어 있으면 참
-		String regEx2 = "^[a-z]+$"; //영문 소문자만 1번 이상.
-		String regEx3 = "^[A-Z]+$"; //영문 대문자만 1번 이상.
-		String regEx4 = "^[a-zA-Z]*$"; //영문 대문자 소문자 만 0번 이상.
-		String regEx5 = "^[가-힣]*$"; //한글만 0번 이상.
-		String regEx6 = "^[a-zA-Z가-힣]*$"; //영문 대문자 소문자 한글만 0번 이상.
-		String regEx7 = "^[a-zA-Z가-힣 ]*$"; //영문 대문자 소문자 한글 공백(space)만  0번 이상.
-		String regEx8 = "^[a-zA-Z가-힣0-9 ]*$"; //영문 대문자 소문자 한글 공백(space)만  0번 이상.
-		String regEx9 = "^[a-zA-Z가-힣0-9-_]*$"; //영문 대문자 소문자 한글 -_ 만  0번 이상.
+		String regEx1 = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\\.[a-z]+$";
+		String regEx2 = "^[a-zA-Z0-9_-]{3,}@[a-zA-Z0-9]+\\.[a-z]+$";
+		String regEx3 = "^[a-zA-Z0-9_-]{3,12}@[a-zA-Z0-9]+\\.[a-z]+$";
+		String regEx4 = "^[a-zA-Z0-9_-]{3,12}@[a-zA-Z0-9]+\\.[a-z]+\\.[a-z]*$";
+		String regEx5 = "^[a-zA-Z0-9_-]{3,12}@[a-zA-Z0-9]+\\.[a-z]+(\\.[a-z])*$"; //사용 안됨
+		String regEx6 = "^[a-zA-Z0-9_-]{3,12}@[a-zA-Z0-9]+(\\.[a-z]+)+$"; //정답
+		String regEx7 = "\\w+@\\w+\\.\\w+"; //\\w 영문 대소문자 / 숫자 /밑줄_/ 허용
+		String regEx8 = "\\w+@[a-zA-Z0-9]+\\.[a-z]+"; //\\w 영문 대소문자 / 숫자 /밑줄_/ 허용
+		String regEx9 = "\\w{3,}@[a-zA-Z0-9]{1,8}\\.[a-z]{1,4}+"; //\\w 영문 대소문자 / 숫자 /밑줄_/ 허용
 		
 		String str00 = "";
-		String str01 = "1234";
-		String str02 = "1234ghjk";
-		String str03 = "aaa";
-		String str04 = "bc";
-		String str05 = "asdc";
-		String str06 = "AS-DF";
-		String str07 = "asDf";
-		String str08 = "abcsDf";
-		String str09 = "abc     "; //space 공백
-		String str10 = "abc			"; //tab 공백
-		String str11 = "ㄱ";
-		String str12 = "그린컴퓨터";
-		String str13 = "그린 컴퓨터";
-		String str14 = "그린 Computer";
-		String str15 = "그린2_Computer2";
+		String str01 = "atom@naver.com";
+		String str02 = "atom1234@naver.com"; 
+		String str03 = "atom_1234@naver.com"; 
+		String str04 = "atom!1234@naver.com"; 
+		String str05 = "atom 1234@naver.com"; 
+		String str06 = "atOM1234123412341234@naver.com"; 
+		String str07 = "a@naver.com"; 
+		String str08 = "at_OM1234@naver.com"; 
+		String str09 = "atom@naver.co.kr"; 
+		String str10 = "_at-om@naver.co.kr"; 
+		String str11 = "_at-om@naver.COM"; 
+		String str12 = "_at-om@naver!.COM"; 
+		String str13 = "_atom@na_ver.com"; 
+		String str14 = "_at-om@na_ver1234.com";
+		String str15 = "atom@naver.comasdf";
 		
-		System.out.println("1:00= : " + Pattern.matches(regEx1, str00)); //한번도 안 나와서 
-		System.out.println("1:01= : " + Pattern.matches(regEx1, str01)); //abc 없음
-		System.out.println("1:02= : " + Pattern.matches(regEx1, str02)); //abc 없음
-		System.out.println("1:03= : " + Pattern.matches(regEx1, str03)); //a 있음
-		System.out.println("1:04= : " + Pattern.matches(regEx1, str04)); //bc 있음
-		System.out.println("1:05= : " + Pattern.matches(regEx1, str05)); // ??
+		System.out.println("1:00= : " + Pattern.matches(regEx1, str00));
+		System.out.println("1:01= : " + Pattern.matches(regEx1, str01));
+		System.out.println("1:02= : " + Pattern.matches(regEx1, str02));
+		System.out.println("1:03= : " + Pattern.matches(regEx1, str03));
+		System.out.println("1:04= : " + Pattern.matches(regEx1, str04));
+		System.out.println("1:05= : " + Pattern.matches(regEx1, str05));
 		System.out.println("1:06= : " + Pattern.matches(regEx1, str06));
 		System.out.println("1:07= : " + Pattern.matches(regEx1, str07));
 		System.out.println("1:08= : " + Pattern.matches(regEx1, str08));
@@ -54,12 +53,12 @@ public class Test4 {
 		System.out.println(); 
 		
 		//화면분활 ctrl shift [
-		System.out.println("2:00= : " + Pattern.matches(regEx2, str00)); // 
-		System.out.println("2:01= : " + Pattern.matches(regEx2, str01)); //
-		System.out.println("2:02= : " + Pattern.matches(regEx2, str02)); //
-		System.out.println("2:03= : " + Pattern.matches(regEx2, str03)); //
-		System.out.println("2:04= : " + Pattern.matches(regEx2, str04)); //
-		System.out.println("2:05= : " + Pattern.matches(regEx2, str05)); //
+		System.out.println("2:00= : " + Pattern.matches(regEx2, str00)); 
+		System.out.println("2:01= : " + Pattern.matches(regEx2, str01)); 
+		System.out.println("2:02= : " + Pattern.matches(regEx2, str02)); 
+		System.out.println("2:03= : " + Pattern.matches(regEx2, str03)); 
+		System.out.println("2:04= : " + Pattern.matches(regEx2, str04)); 
+		System.out.println("2:05= : " + Pattern.matches(regEx2, str05)); 
 		System.out.println("2:06= : " + Pattern.matches(regEx2, str06));
 		System.out.println("2:07= : " + Pattern.matches(regEx2, str07));
 		System.out.println("2:08= : " + Pattern.matches(regEx2, str08));
@@ -72,12 +71,12 @@ public class Test4 {
 		System.out.println("2:15= : " + Pattern.matches(regEx2, str15));
 		System.out.println(); 
 		
-		System.out.println("3:00= : " + Pattern.matches(regEx3, str00)); // 
-		System.out.println("3:01= : " + Pattern.matches(regEx3, str01)); //
-		System.out.println("3:02= : " + Pattern.matches(regEx3, str02)); //
-		System.out.println("3:03= : " + Pattern.matches(regEx3, str03)); //
-		System.out.println("3:04= : " + Pattern.matches(regEx3, str04)); //
-		System.out.println("3:05= : " + Pattern.matches(regEx3, str05)); //
+		System.out.println("3:00= : " + Pattern.matches(regEx3, str00)); 
+		System.out.println("3:01= : " + Pattern.matches(regEx3, str01)); 
+		System.out.println("3:02= : " + Pattern.matches(regEx3, str02)); 
+		System.out.println("3:03= : " + Pattern.matches(regEx3, str03)); 
+		System.out.println("3:04= : " + Pattern.matches(regEx3, str04)); 
+		System.out.println("3:05= : " + Pattern.matches(regEx3, str05)); 
 		System.out.println("3:06= : " + Pattern.matches(regEx3, str06));
 		System.out.println("3:07= : " + Pattern.matches(regEx3, str07));
 		System.out.println("3:08= : " + Pattern.matches(regEx3, str08));
--- src/api8_Date/T4_DateCompare.java
+++ src/api8_Date/T4_DateCompare.java
@@ -5,14 +5,14 @@ import java.time.temporal.ChronoUnit;
 
 public class T4_DateCompare {
 	public static void main(String[] args) {
-//		LocalDateTime startDateTime = LocalDateTime.now();
-//		LocalDateTime endDateTime = LocalDateTime.of(2024, 12, 31, 0, 0, 0);
-//		
-//		System.out.println("남은 년도 : " + startDateTime.until(endDateTime, ChronoUnit.YEARS));
-//		System.out.println("남은 월 : " + startDateTime.until(endDateTime, ChronoUnit.MONTHS));
-//		System.out.println("남은 일 : " + startDateTime.until(endDateTime, ChronoUnit.DAYS));
-//		System.out.println("남은 시간 : " + startDateTime.until(endDateTime, ChronoUnit.HOURS));
-//		System.out.println("남은 분 : " + startDateTime.until(endDateTime, ChronoUnit.MINUTES));
-//		System.out.println("남은 초 : " + startDateTime.until(endDateTime, ChronoUnit.SECONDS));
+		LocalDateTime startDateTime = LocalDateTime.now();
+		LocalDateTime endDateTime = LocalDateTime.of(2024, 12, 31, 0, 0, 0);
+		
+		System.out.println("남은 년도 : " + startDateTime.until(endDateTime, ChronoUnit.YEARS));
+		System.out.println("남은 월 : " + startDateTime.until(endDateTime, ChronoUnit.MONTHS));
+		System.out.println("남은 일 : " + startDateTime.until(endDateTime, ChronoUnit.DAYS));
+		System.out.println("남은 시간 : " + startDateTime.until(endDateTime, ChronoUnit.HOURS));
+		System.out.println("남은 분 : " + startDateTime.until(endDateTime, ChronoUnit.MINUTES));
+		System.out.println("남은 초 : " + startDateTime.until(endDateTime, ChronoUnit.SECONDS));
 	}
 }
